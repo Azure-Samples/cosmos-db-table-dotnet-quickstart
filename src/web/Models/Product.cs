5ef5760f75898134b48c96b03a3dc4d3ad80d46d
Sidney Andrews <sidandrews@microsoft.com>
2024-10-08 21:58:02 -0400

    Fix application code

--- src/web/Models/Product.cs
+++ src/web/Models/Product.cs
@@ -4,16 +4,19 @@ using Azure.Data.Tables;
 namespace Cosmos.Samples.Table.Quickstart.Web.Models;
 
 // <model>
-public record Product(
-    string name,
-    int quantity,
-    decimal price,
-    bool clearance
-) : ITableEntity
+public record Product : ITableEntity
 {
     public string RowKey { get; set; } = $"{Guid.NewGuid()}";
 
     public string PartitionKey { get; set; } = String.Empty;
+
+    public string Name { get; set; } = String.Empty;
+
+    public int Quantity { get; set; } = 0;
+
+    public decimal Price { get; set; } = 0.0m;
+
+    public bool Clearance { get; set; } = false;
 
     public ETag ETag { get; set; } = ETag.All;
 
